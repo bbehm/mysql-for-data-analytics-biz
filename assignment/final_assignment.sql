275928f1e32b1842788947b042b48b77f08b23fd
Bianca Behm <bbehm@student.hive.fi>
2020-10-16 14:32:45 +0300

    final tasks

--- assignment/final_assignment.sql
+++ assignment/final_assignment.sql
@@ -160,3 +160,134 @@ AND weekday(Data_received) = 4
 AND DATEDIFF(Data_sent_to_company, Data_received) > 5
 GROUP BY company
 ORDER BY frequency DESC;
+
+/*
+** Exercise 8
+** Which product produces highest revenue?
+*/
+
+/*
+** The buy and sell prices vary for the same products which makes it a bit more complicated.
+** First I create a view where I add a revenue column as the difference between buy and sell price
+** and join the columns from two tables based on productCode.
+*/
+
+CREATE VIEW info AS
+SELECT orderdetails.productCode, products.productName, orderdetails.quantityOrdered, ROUND((priceEach - products.buyPrice),2) AS revenue
+FROM orderdetails
+LEFT JOIN products ON products.productCode=orderdetails.productCode;
+
+/*
+** For the second view I add the total revenue column where I multiply the revenue for a single product with
+** the quantity ordered
+*/
+
+CREATE VIEW info2 AS
+SELECT productName, productCode, revenue, SUM(quantityOrdered) AS quantity, (quantityOrdered * revenue) AS total_revenue FROM info
+GROUP BY productName, productCode, revenue, quantityOrdered
+ORDER BY productName;
+
+/*
+** Finally we sum up the total revenue for data points regarding the same product to get the final amount
+*/
+
+SELECT productName, SUM(total_revenue) as total_revenue
+FROM info2
+GROUP BY productName
+ORDER BY total_revenue DESC;
+
+/*
+** Exercise 9
+*/
+
+/*
+** 9.1 Likelihood for customers from  different cuuntries to become
+** re-patronage travelers. Re-patronage customers / total customers
+*/
+
+/*
+** First I create a more manageable view
+*/
+
+CREATE VIEW repat AS
+SELECT repatronage_review_assignment.user_id, repatronage_user_assignment.country, COUNT(*) as frequency
+FROM repatronage_review_assignment
+LEFT JOIN repatronage_user_assignment ON repatronage_review_assignment.user_id=repatronage_user_assignment.user_id
+GROUP BY user_id, country
+ORDER BY frequency DESC;
+
+/*
+** Using nested SELECT statements to get the likelihood --> 0,0875
+*/
+
+SELECT ((SELECT COUNT(*)
+FROM repat
+WHERE frequency > 1 and country = 'Denmark') / (SELECT COUNT(*)
+FROM repat
+WHERE country = 'Denmark'));
+
+/*
+** Same for Austria --> 0,0503
+*/
+
+SELECT ((SELECT COUNT(*)
+FROM repat
+WHERE frequency > 1 and country = 'Austria') / (SELECT COUNT(*)
+FROM repat
+WHERE country = 'Austria'));
+
+/*
+** 9.2 Comparing average ratings between first patronage and second patronage
+*/
+
+/*
+** First I create a table with the first visit data - only taking into
+** consideration re-patronage visitors.
+*/
+
+CREATE TABLE first_visit_average (SELECT user_id, hotel_id, MIN(review_date)
+AS first_visit, COUNT(review_date) AS freq, overall_rating, rooms_rating, service_rating, location_rating, value_rating
+FROM repatronage_review_assignment
+WHERE user_id IS NOT NULL
+GROUP BY user_id, hotel_id, overall_rating, rooms_rating, service_rating, location_rating, value_rating
+HAVING freq > 1
+ORDER BY freq DESC);
+
+/*
+** Then I show the average ratings from all of the reviews in that category
+*/
+
+SELECT AVG(overall_rating), AVG(rooms_rating), AVG(service_rating), AVG(location_rating), AVG(value_rating)
+FROM first_visit_average;
+
+/*
+** Then I create a table for the second visit reviews
+*/
+
+CREATE TABLE second_visit_average (SELECT rra.user_id, rra.hotel_id, MIN(rra.review_date) AS second_visit, rra.overall_rating, rra.rooms_rating, rra.location_rating, rra.service_rating, rra.value_rating
+FROM repatronage_review_assignment rra, first_visit_average fv
+WHERE rra.review_date != fv.first_visit AND rra.user_id = fv.user_id
+GROUP BY rra.user_id, rra.hotel_id, rra.overall_rating, rra.rooms_rating, rra.location_rating, rra.service_rating, rra.value_rating);
+
+/*
+** and finally showing the average of those
+*/
+
+SELECT AVG(overall_rating), AVG(rooms_rating), AVG(service_rating), AVG(location_rating), AVG(value_rating)
+FROM second_visit_average;
+
+/*
+** 9.3 Which hotel has shorter average time between first and
+** second visit?
+*/
+
+/*
+** Here I use the tables created in question 9.2 with the respective
+** samples and use datediff to calculate the average difference
+*/
+
+SELECT fva.hotel_id, AVG(DATEDIFF(second_visit, first_visit)) AS average_difference
+FROM first_visit_average fva
+INNER JOIN second_visit_average sva
+ON fva.user_id = sva.user_id
+GROUP BY hotel_id;
